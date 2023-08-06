2333487b5e9bfe75ec842d3825c699e3c9af171c
Tirth <tirthacodes@gmail.com>
2023-08-06 17:15:14 +0545

    nested route

--- src/users/controller/users/users.controller.ts
+++ src/users/controller/users/users.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    @Get()
+    getUsers(){
+        return [{username: 'Tirtha', email: 'gmail@gmail.com'}]
+    }
+
+    @Get('posts')
+    getUsersPosts(){
+        return [
+        {
+            username: 'Tirtha',
+            email: 'gmail@gmail.com',
+            posts: [{
+                id:1, 
+                title: 'Post 1',
+            },
+            {
+                id:2,
+                title:'Post 2',
+            },
+            ],
+        }]
+    }
+
+    @Get('posts/comments')
+    getUserPostComments(){
+        return[
+            {
+                id: 1,
+                title: 'Post 1',
+                comments: []
+            }
+        ]
+    }
+}
