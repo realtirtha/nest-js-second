5f88e83b839b637c21985409241641e80d1c843d
Tirth <tirthacodes@gmail.com>
2023-08-06 17:43:14 +0545

    implemented validatiaon

--- src/users/dtos/CreateUser.dto.ts
+++ src/users/dtos/CreateUser.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
 export class CreateUserDto{
+    @IsNotEmpty()
     username: String;
+
+    @IsEmail()
+    @IsNotEmpty()
     email:  String;
 }
