5c8b7f1c6d4afb898357ec7195d1c9a539e42ade
Tirth <tirthacodes@gmail.com>
2023-08-06 17:34:21 +0545

    sorted in ascending using query param

--- src/users/controller/users/users.controller.ts
+++ src/users/controller/users/users.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
 
 @Controller('users')
 export class UsersController {
 
     @Get()
-    getUsers(){
+    getUsers(@Query('sortBy') sortBy: string){
+        console.log(sortBy);
         return [{username: 'Tirtha', email: 'gmail@gmail.com'}]
     }
+    
 
     @Post('create')
     createUser(@Body() userData: CreateUserDto){
@@ -15,10 +17,10 @@ export class UsersController {
         return {};
     }
 
-    @Get(':id/:postId')
-    getUserById(@Param('id') id: string, @Param('postId') postId: string){
+    @Get(':id')
+    getUserById(@Param('id') id: string){
         console.log(id);
-        return{id, postId};
+        return{id};
 
     }
 }
