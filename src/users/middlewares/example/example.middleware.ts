d5c6bf1366634703a3aa9f2d63ca95e03f297fdd
Tirth <tirthacodes@gmail.com>
2023-08-07 11:02:19 +0545

    middlewares done + on headers authorization

---
+++ src/users/middlewares/example/example.middleware.ts
@@ -0,0 +1,19 @@
+import { Injectable, NestMiddleware, HttpException, HttpStatus } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class ExampleMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log('Example Middleware');
+    console.log(req.headers.authorization);
+    const {authorization} = req.headers;
+
+    if(!authorization)
+      throw new HttpException('No Authorization Token', HttpStatus.FORBIDDEN);
+
+    if (authorization == 'sks') next();
+    else
+      throw new HttpException(
+    'Invalid Authorization Token', HttpStatus.FORBIDDEN)
+  }
+}
